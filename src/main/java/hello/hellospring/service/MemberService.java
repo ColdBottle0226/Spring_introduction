73e9f64c2e1be57f30a2532d6539564e75edd521
chan <3233978@naver.com>
2022-06-23 13:48:01 +0900

    JPA

--- src/main/java/hello/hellospring/service/MemberService.java
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -4,11 +4,13 @@ import hello.hellospring.domain.Member;
 import hello.hellospring.repository.MemberRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
 
 //@Service    // MemberService는 순수 자바 클래스라 스프링이 모른다.
+@Transactional
 public class MemberService {
 
     private final MemberRepository memberRepository;
@@ -21,6 +23,7 @@ public class MemberService {
     /*
     회원 가입
      */
+
     public Long join(Member member) {
         // 같은 이름이 있는 중복 회원 X
         validateDuplicateMember(member);
